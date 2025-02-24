2d208fd17a921e42c727da1fedef2e3655e5d328
Debra Lewis <debra.lewis@p3-tech.com>
2025-02-24 12:33:14 -0500

    Add project files.

---
+++ CaliburnMicroKeyBindingEnablingTest/Bootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using CaliburnMicroKeyBindingEnablingTest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CaliburnMicroKeyBindingEnablingTest
+{
+    internal class Bootstrapper : BootstrapperBase
+    {
+        private readonly SimpleContainer _container;
+
+        public Bootstrapper()
+        {
+            _container = new SimpleContainer();
+            Initialize();
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+            => _container.GetAllInstances(service);
+
+        protected override object GetInstance(Type service, string key)
+            => _container.GetInstance(service, key);
+
+        protected override void BuildUp(object instance)
+            => _container.BuildUp(instance);
+
+        protected override void Configure()
+        {
+            _container.Singleton<IEventAggregator, EventAggregator>();
+            _container.Singleton<IWindowManager, WindowManager>();
+
+            _container.PerRequest<IShellViewModel, ShellViewModel>();
+            base.Configure();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<IShellViewModel>();
+        }
+    }
+}
---
+++ CaliburnMicroKeyBindingEnablingTest/ViewModels/IShellViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CaliburnMicroKeyBindingEnablingTest.ViewModels
+{
+    internal interface IShellViewModel
+    {
+        bool CanDoThing { get; }
+        void DoThing();
+        void ChangeEnabledForDoThing();
+    }
+
+    internal class ShellViewModel : Screen, IShellViewModel
+    {
+        private bool _canDoThing = true;
+
+        public ShellViewModel()
+        {
+
+        }
+
+        public bool CanDoThing
+        {
+            get => _canDoThing;
+            private set => Set(ref _canDoThing, value);
+        }
+
+        public void DoThing()
+        {
+            MessageBox.Show("I did a thing!");
+        }
+
+        public void ChangeEnabledForDoThing()
+        {
+            CanDoThing = !CanDoThing;
+        }
+    }
+}
