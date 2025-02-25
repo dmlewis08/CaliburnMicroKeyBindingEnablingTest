92ad9b2076981aeb0315ebfdcb4b983bae9819a6
Debra Lewis <debra.lewis@p3-tech.com>
2025-02-25 10:30:44 -0500

    New branch with more versions of the binding methods

--- CaliburnMicroKeyBindingEnablingTest/ViewModels/IShellViewModel.cs
+++ CaliburnMicroKeyBindingEnablingTest/ViewModels/IShellViewModel.cs
@@ -13,11 +13,22 @@ namespace CaliburnMicroKeyBindingEnablingTest.ViewModels
         bool CanDoThing { get; }
         void DoThing();
         void ChangeEnabledForDoThing();
+        Task DoAsyncThingAsync();
+        bool CanDoAsyncThing { get; }
+        void DoSecondThing();
+        void ChangeEnabledForDoAsyncThing();
+        void ChangeEnabledForDoSecondThing();
+
+        bool CanDoSecondThing { get; }
+
+        Task DoSecondAsyncThingAsync();
+        bool CanDoSecondAsyncThing { get; }
+        void ChangeEnabledForSecondAsyncThing();
     }
 
     internal class ShellViewModel : Screen, IShellViewModel
     {
-        private bool _canDoThing = true;
+        private bool _canDoThing = true, _canDoAsyncThing = true, _canDoSecondThing = true, _canDoSecondAsyncThing = true;
 
         public ShellViewModel()
         {
@@ -38,6 +49,54 @@ namespace CaliburnMicroKeyBindingEnablingTest.ViewModels
         public void ChangeEnabledForDoThing()
         {
             CanDoThing = !CanDoThing;
+        }
+
+        public async Task DoAsyncThingAsync()
+        {
+            await Task.Run(() => MessageBox.Show("I did an async thing!"));
+        }
+
+        public bool CanDoAsyncThing
+        {
+            get => _canDoAsyncThing;
+            private set => Set(ref _canDoAsyncThing, value);
+        }
+
+        public void ChangeEnabledForDoAsyncThing()
+        {
+            CanDoAsyncThing = !CanDoAsyncThing;
+        }
+
+        public void DoSecondThing()
+        {
+            MessageBox.Show("I did a second thing!");
+        }
+
+        public bool CanDoSecondThing
+        {
+            get => _canDoSecondThing;
+            private set => Set(ref _canDoSecondThing, value);
+        }
+
+        public void ChangeEnabledForDoSecondThing()
+        {
+            CanDoSecondThing = !CanDoSecondThing;
+        }
+
+        public async Task DoSecondAsyncThingAsync()
+        {
+            await Task.Run(() => MessageBox.Show("I did a second async thing!"));
+        }
+
+        public bool CanDoSecondAsyncThing
+        {
+            get => _canDoSecondAsyncThing;
+            private set => Set(ref _canDoSecondAsyncThing, value);
+        }
+
+        public void ChangeEnabledForSecondAsyncThing()
+        {
+            CanDoSecondAsyncThing = !CanDoSecondAsyncThing;
         }
     }
 }
